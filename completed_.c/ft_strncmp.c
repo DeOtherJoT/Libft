7f706e445ee6330f88be4c7d354a9c67956bccbd
Joshua Thor <jthor@u81z05s05.42kl.edu.my>
2021-10-13 22:21:40 +0800

    Modified strncmp

--- completed_.c/ft_strncmp.c
+++ completed_.c/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jthor <jthor@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 21:40:31 by jthor             #+#    #+#             */
-/*   Updated: 2021/10/12 21:05:34 by jthor            ###   ########.fr       */
+/*   Updated: 2021/10/13 22:05:54 by jthor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	while (i < n)
 	{
-		if (s1[i] != s2[i])
+		if ((s1[i] != s2[i]) || !(s1[i] && s2[i]))
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
