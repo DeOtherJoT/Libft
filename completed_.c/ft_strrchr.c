6f0954f3aa895809a6996da57b761e6fe48f8db8
Joshua Thor <jthor@u81z05s05.42kl.edu.my>
2021-10-13 18:55:17 +0800

    Fixed strchr and strrchr

--- completed_.c/ft_strrchr.c
+++ completed_.c/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jthor <jthor@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 21:41:03 by jthor             #+#    #+#             */
-/*   Updated: 2021/09/29 21:41:10 by jthor            ###   ########.fr       */
+/*   Updated: 2021/10/13 18:52:47 by jthor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@ char	*ft_strrchr(const char *s, int c)
 	char	*ret;
 
 	ret = 0;
+	if (c >= 256)
+		c -= 256;
 	while (*s)
 	{
 		if (*s == c)
