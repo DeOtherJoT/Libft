fd078f95ec8525d51570f62c1228d8ba116a7bc5
Joshua Thor <jthor@u81z05s05.42kl.edu.my>
2021-10-15 18:18:02 +0800

    Modified memcmp

--- completed_.c/ft_memcmp.c
+++ completed_.c/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jthor <jthor@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 21:37:41 by jthor             #+#    #+#             */
-/*   Updated: 2021/10/13 16:07:03 by jthor            ###   ########.fr       */
+/*   Updated: 2021/10/13 16:34:05 by jthor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	if (!n)
 		return (0);
-	while ((*(unsigned char *)s1 == *(unsigned char *)s2) && n > 0)
+	while ((*(unsigned char *)s1 == *(unsigned char *)s2) && --n)
 	{
 		s1++;
 		s2++;
-		n--;
 	}
 	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
